a7d885c86c873f0df9c20c46be09fad02ce7e79d
Graham Otte <graham.otte@gmail.com>
2015-10-29 19:14:55 -0400

    added defect 14

--- bxapp/urls.py
+++ bxapp/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='overview'),
   url(r'^overview/$', views.overview, name='overview'),
-  url(r'^signUp/$', views.signUp, name='signUp'),
-  url(r'^signin/$', views.signin, name='signin'),
+  url(r'^signin/$', views.signUp, name='signUp'),
+  url(r'^signUp/$', views.signin, name='signin'),
   url(r'^purchase/$', views.purchase, name='purchase'),
   url(r'^addBook/$', views.addBook, name='addBook'),
   url(r'^bookAdded/$', views.addBook, name='bookAdded')
